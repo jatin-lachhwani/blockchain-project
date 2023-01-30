f6c3e6cbca9d5ffeb371afed63074194170316a2
jatin lachhwani <jatin.commit@gmail.com>
2023-01-30 21:59:02 +0530

    network generated correctly with test function

--- create_network.py
+++ create_network.py
@@ -1,15 +1,5 @@
 from random import randint
 from config import L_CONN,H_CONN
-def random_peers(source,n):
-    num_conn=randint(L_CONN,H_CONN)
-    conn_set=set()
-    while len(conn_set)<num_conn:
-        peer=randint(0,n-1);
-        if peer != source:
-            conn_set.add(peer)
-    lst=list(conn_set)
-    lst.sort()
-    return lst
 
 def check_connected(lst,n):
     visited=[0]*n
@@ -27,16 +17,44 @@ def check_connected(lst,n):
             return False
     return True
 
-def create_connected_network(n):
+def check_least_conn(var):
+    for i in var:
+        if i<L_CONN:
+            return True
+    return False
+
+def create_network(n):
+    lst = [[] for i in range(n)]
+    var = [0]*n
     connected=False
-    lst=[]
     while not connected:
-        e_lst=[]
-        lst= [e_lst for i in range(n)]
-        lst = [random_peers(i,n) for i in range(n)]
+        while check_least_conn(var):
+            x,y=randint(0,n-1),randint(0,n-1)
+            if x==y : 
+                continue
+            if x not in lst[y]:
+                if len(lst[x]) < H_CONN and len(lst[y]) < H_CONN:
+                    lst[x].append(y)
+                    var[x]+=1
+                    lst[y].append(x)
+                    var[y]+=1
+            print("loop1")
+        print("loop2")
         connected=check_connected(lst,n)
-        if not connected:
-            lst=[]
+        if connected==False:
+            lst = [[] for i in range(n)]
+            var = [0]*n
     return lst
 
-print(create_connected_network(10))
+############# For testing ##############
+# for i in range(100):
+#     lst=create_network(randint(10,30))
+#     for x in lst:
+#         if len(x)>8:
+#             print(lst,"len>8")
+#         if len(x)==8:
+#             print(lst,"len=8")
+#         if len(x)<4:
+#             print(lst,"len<4")
+#         if len(x)==4:
+#             print(lst,"len=4")
